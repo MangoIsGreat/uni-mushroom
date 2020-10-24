cb5f417ea1167a8e81bd790511618f398f995aac
zp <859247268@qq.com>
2020-10-25 04:29:30 +0800

    feat: 创建login页面

---
+++ src/pages/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <view>
+        <text>登陆页面----{{msg}}</text>
+    </view>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+export default Vue.extend({
+    data() {
+        return {
+            msg: "login"
+        }
+    }
+})
+</script>
