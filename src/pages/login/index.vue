2c503c7632aa3d7f3afa019d48886991a8fab24f
zp <859247268@qq.com>
2020-10-25 17:10:57 +0800

    feat: 微信授权登录

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,16 +1,128 @@
 <template>
-    <view>
-        <text>登陆页面----{{msg}}</text>
+  <view>
+    <view class="login-container">
+      <image class="logo" src="/static/images/logo@2x.png" alt="" />
+      <text class="title">蘑菇在线</text>
+      <image class="login-tips" src="/static/images/login_tips@2x.png" alt="" />
+      <button
+        open-type="getUserInfo"
+        @getuserinfo="wxLogin"
+        plain
+        class="wx-login-button"
+      >
+        <image
+          class="wx-login-img"
+          src="/static/images/wx_login@2x.png"
+          alt=""
+        />
+      </button>
+      <text @click="phoneLogin" class="phone-login">手机号登录</text>
+      <text class="bottom-tip">Copyright © 2020 蘑菇在线 </text>
     </view>
+  </view>
 </template>
 
 <script lang="ts">
-import Vue from "vue"
+import Vue from "vue";
 export default Vue.extend({
-    data() {
-        return {
-            msg: "login"
-        }
-    }
-})
+  data() {
+    return {
+      msg: "login",
+    };
+  },
+  methods: {
+    wxLogin(e: any) {
+      console.log(e);
+      //   如果用户拒绝，则直接返回
+      if (e.detail.errMsg === "getUserInfo:fail auth deny") return;
+
+      // 结构头像和昵称
+      const { avatarUrl: avatar, nickName: nickname } = e.detail.userInfo;
+
+      // 调用uni.login
+      uni.login({
+        success: ({ code }) => {
+          console.log(avatar, nickname, code);
+        },
+      });
+    },
+  },
+});
 </script>
+
+<style lang="less" scoped>
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  .logo {
+    margin-top: 180rpx;
+    width: 212rpx;
+    height: 212rpx;
+  }
+  .title {
+    color: #333333;
+    font-size: 24px;
+    margin-top: 20rpx;
+  }
+  .login-tips {
+    margin-top: 30rpx;
+    height: 34rpx;
+    width: 292rpx;
+    position: relative;
+  }
+  .wx-login-button {
+    margin-top: 200rpx;
+    width: 568rpx;
+    height: 98rpx;
+    padding: 0;
+    border: none;
+  }
+  .wx-login-img {
+    // margin-top:200rpx;
+    width: 568rpx;
+    height: 98rpx;
+  }
+  .phone-login {
+    margin-top: 40rpx;
+    color: #333333;
+    font-size: 15px;
+  }
+  .bottom-tip {
+    color: #a8a8a8;
+    font-size: 10px;
+    position: absolute;
+    bottom: 40rpx;
+  }
+}
+.wx-login-container {
+  position: relative;
+  .wx-login-tip1 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 450rpx;
+    padding-top: 50rpx;
+    img {
+      width: 198rpx;
+      height: 198rpx;
+    }
+    p {
+      color: #bebebe;
+      margin-top: 50rpx;
+      font-size: 13px;
+    }
+  }
+  .wx-login-tip1-cover {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    height: 500rpx;
+    background: rgba(0, 0, 0, 1);
+    opacity: 0.16;
+  }
+}
+</style>
