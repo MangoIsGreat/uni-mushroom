6232e4aedfbad30713e91808fadc631e6994c973
zp <859247268@qq.com>
2020-10-27 11:07:41 +0800

    feat:获取课程详情页面的数据

--- src/pages/course-detail/index.vue
+++ src/pages/course-detail/index.vue
@@ -6,14 +6,28 @@
 
 <script lang="ts">
 import Vue from "vue";
+import { fetch } from "../../utils/fetch";
 export default Vue.extend({
   data() {
     return {
-      id: null,
+      id: null, // 课程Id
+      course_detail: null, // 课程详情
     };
   },
   onLoad(options) {
     this.id = options.id;
+
+    // 发送请求,获取课程详情数据
+    this.getCourseDetailData();
+  },
+  methods: {
+    async getCourseDetailData() {
+      const result = await fetch({ url: `course/${this.id}` });
+
+      if (result.data.status === 0) {
+        this.course_detail = result.data.message;
+      }
+    },
   },
 });
 </script>
